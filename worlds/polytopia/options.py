fc3452cad59f71b9a7a25def5834128820486f4a
Eclassic32 <eclassic32004@gmail.com>
2026-08-22 14:52:30 +0500

    added FirstUnlockedTribe and turned most ranges into NamedRange

--- worlds/polytopia/options.py
+++ worlds/polytopia/options.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from Options import OptionGroup, OptionSet, PerGameCommonOptions, Range, Toggle
+from Options import Choice, DefaultOnToggle, NamedRange, OptionGroup, OptionSet, PerGameCommonOptions, Range, Toggle
 
 from .strings import REGULAR_TRIBE_NAMES, TRIBE_NAMES
 
@@ -17,6 +17,17 @@ class PlayableTribes(OptionSet):
     valid_keys = TRIBE_NAMES
     default = REGULAR_TRIBE_NAMES
 
+class FirstUnlockedTribe(Choice):
+    """The first tribe that will be unlocked for the player.
+    Notice: Client mod does NOT let you select tribes that you do not own in main game.
+
+    Possible values: {TRIBE_NAMES}
+    """
+
+    display_name = "First Unlocked Tribe"
+    valid_keys = TRIBE_NAMES
+    default = "Xinxi"
+
 # Goal Options
 class RequiredUniqueTribesWins(Range):
     """Amount of wins with unique tribes required for a player to goal the game.
@@ -29,7 +40,7 @@ class RequiredUniqueTribesWins(Range):
     range_end = 16
     default = 4
 
-class RequiredScoreForVictory(Range):
+class RequiredScoreForVictory(NamedRange):
     """Score required for a tribe to be considered as victorious.
 
     Rating in the main game:
@@ -44,9 +55,14 @@ class RequiredScoreForVictory(Range):
     range_start = 1
     range_end = 100
     default = 50
+    special_range_names = {  # noqa: RUF012
+        "1_Star": 10,
+        "2_Stars": 25,
+        "3_Stars": 50
+    }
 
 # Score Check Options
-class ScoreChecksMin(Range):
+class ScoreChecksMin(NamedRange):
     """Minimum score*1000 for score checks.
     Setting ScoreChecksMin and ScoreChecksMax to 0 will disable score checks.
 
@@ -62,8 +78,13 @@ class ScoreChecksMin(Range):
     range_start = 0
     range_end = 100
     default = 10
+    special_range_names = {  # noqa: RUF012
+        "1_Star": 10,
+        "2_Stars": 25,
+        "3_Stars": 50
+    }
 
-class ScoreChecksMax(Range):
+class ScoreChecksMax(NamedRange):
     """Maximum score*1000 for score checks.
     Setting ScoreChecksMin and ScoreChecksMax to 0 will disable score checks.
 
@@ -79,19 +100,24 @@ class ScoreChecksMax(Range):
     range_start = 0
     range_end = 100
     default = 50
+    special_range_names = {  # noqa: RUF012
+        "1_Star": 10,
+        "2_Stars": 25,
+        "3_Stars": 50
+    }
 
 class ScoreChecksStep(Range):
-    """Step size for score checks.
+    """Step size for score checks. (*1000)
 
     min = 1, max = 100, default = 10
     """
 
-    display_name = "Score Checks Step Size"
+    display_name = "Score Checks Step Size (*1000)"
     range_start = 1
     range_end = 100
     default = 10
 
-class ShouldSendScoreChecksImmediately(Toggle):
+class ShouldSendScoreChecksImmediately(DefaultOnToggle):
     """Whether to send score checks immediately or wait until the end of the match.
 
     True - Send score checks immediately
@@ -99,11 +125,10 @@ class ShouldSendScoreChecksImmediately(Toggle):
     """
 
     display_name = "Send Score Checks Immediately"
-    default = True
 
 polytopia_option_groups = [
     OptionGroup("Goal Options", [RequiredUniqueTribesWins, RequiredScoreForVictory]),
-    OptionGroup("Playable Tribes", [PlayableTribes]),
+    OptionGroup("Playable Tribes", [PlayableTribes, FirstUnlockedTribe]),
     OptionGroup("Score Check Options", [ScoreChecksMin, ScoreChecksMax,
                                         ScoreChecksStep, ShouldSendScoreChecksImmediately]),
 ]
@@ -113,6 +138,7 @@ class PolytopiaOptions(PerGameCommonOptions):
     unique_tribes_wins: RequiredUniqueTribesWins
     score_for_victory: RequiredScoreForVictory
     playable_tribes: PlayableTribes
+    first_unlocked_tribe: FirstUnlockedTribe
     score_checks_min: ScoreChecksMin
     score_checks_max: ScoreChecksMax
     score_checks_step: ScoreChecksStep
